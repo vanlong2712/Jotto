2022d47133e97d51ba80dea32fcd39e1a1c12961
Long Dang <longdv2@vng.com.vn>
2019-12-30 12:23:46 +0700

    setup Input component and storeFactory test utils

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,10 @@
 import checkPropTypes from 'check-prop-types';
+import {createStore} from 'redux';
+import rootReducer from '../src/reducers';
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
 
 /**
  *
