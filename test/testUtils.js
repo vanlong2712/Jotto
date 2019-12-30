0304aca0f0e8ff46a0a8213e2d97551b97c9060f
Long Dang <longdv2@vng.com.vn>
2019-12-30 14:49:45 +0700

    install redux thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,9 +1,11 @@
 import checkPropTypes from 'check-prop-types';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../src/reducers';
+import {middlewares} from "../src/configureStore";
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
